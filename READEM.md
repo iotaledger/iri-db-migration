56f888d14f8d13c981f6fe77446e5307c6353154
Acha Bill <achabill12@gmail.com>
2020-05-26 11:41:02 +0100

    initial commit

---
+++ READEM.md
@@ -0,0 +1,21 @@
+# Broadcast all txs from IRI to its neighbors
+
+This is a tool to migrate IRI databasae to hornet. 
+
+It reads all transactions from the database and broadcasts them to the connected neighbor.  
+If you are peered with more than 1 neighbor, it'll broadcast only to the 1st one.
+
+N.B Hornet will not accept transactions of which their timestamp is older than the milestone of the used local snapshot. If the hornet nodes starts from a global snapshot, the timestamp validation will always pass.
+
+## Running
+Copy thie script into your iris' `/ixi` directory such that you have `ixi/broadcast-db.ixi/{index.js, package.json}.`. The script will start automatically.
+
+You should see logs such as 
+
+```
+05/26 11:34:01.080 [Thread-2] INFO  DB-Migration:38 - Gossiping tx 0 of 26751. Time elapsed: 0 Days 0 Hours 0 Minutes 0 Seconds
+05/26 11:34:01.087 [Thread-2] INFO  DB-Migration:38 - Gossiping tx 100 of 26751. Time elapsed: 0 Days 0 Hours 0 Minutes 0 Seconds
+05/26 11:34:01.092 [Thread-2] INFO  DB-Migration:38 - Gossiping tx 200 of 26751. Time elapsed: 0 Days 0 Hours 0 Minutes 0 Seconds
+05/26 11:34:01.097 [Thread-2] INFO  DB-Migration:38 - Gossiping tx 300 of 26751. Time elapsed: 0 Days 0 Hours 0 Minutes 0 Seconds
+05/26 11:34:01.110 [Thread-2] INFO  DB-Migration:38 - Gossiping tx 400 of 26751. Time elapsed: 0 Days 0 Hours 0 Minutes 0 Seconds
+```
---
+++ index.js
@@ -0,0 +1,66 @@
+var System = java.lang.System;
+var LoggerFactory = org.slf4j.LoggerFactory;
+var Thread = java.lang.Thread;
+var Collectors = java.util.stream.Collectors;
+var SimpleDateFormat = java.text.SimpleDateFormat;
+var Date = java.util.Date;
+var TimeUnit = java.util.concurrent.TimeUnit;
+var StringBuilder = java.lang.StringBuilder;
+
+var iri = com.iota.iri;
+var TransactionViewModel = iri.controllers.TransactionViewModel;
+var tangle = IOTA.tangle;
+var SLEEP_SECS = 5;
+
+
+var log = LoggerFactory.getLogger("DB-Migration");
+
+var neighborRouter = IOTA.neighborRouter;
+var tvm = TransactionViewModel.first(tangle);
+var startTime = System.currentTimeMillis();
+var totalTxsCount = TransactionViewModel.getNumberOfStoredTransactions(tangle);
+var i = 0;
+
+while (tvm != null) {
+    var neighborList = neighborRouter.getConnectedNeighbors().values().stream().collect(Collectors.toList());
+    if (neighborList.size() == 0) {
+        //neighbor is not connected. Wait a bit
+        System.out.println("Waiting for neighbor");
+        Thread.sleep(SLEEP_SECS * 1000);
+        continue;
+    }
+    if (neighborList.size > 1) {
+        log.info("Broadcasting only to the first neighbor");
+    }
+    if (i % 100 == 0) {
+        var now = System.currentTimeMillis();
+        var elapsed = now - startTime;
+        log.info("Gossiping tx " + i + " of " + totalTxsCount + ". Time elapsed: " + getDurationBreakdown(elapsed));
+    }
+    var neighbor = neighborList.get(0);
+    neighborRouter.gossipTransactionTo(neighbor, tvm);
+    tvm = tvm.next(tangle);
+    i++;
+}
+
+function getDurationBreakdown(millis) {
+    var days = TimeUnit.MILLISECONDS.toDays(millis);
+    millis -= TimeUnit.DAYS.toMillis(days);
+    var hours = TimeUnit.MILLISECONDS.toHours(millis);
+    millis -= TimeUnit.HOURS.toMillis(hours);
+    var minutes = TimeUnit.MILLISECONDS.toMinutes(millis);
+    millis -= TimeUnit.MINUTES.toMillis(minutes);
+    var seconds = TimeUnit.MILLISECONDS.toSeconds(millis);
+
+    var sb = new StringBuilder(64);
+    sb.append(days);
+    sb.append(" Days ");
+    sb.append(hours);
+    sb.append(" Hours ");
+    sb.append(minutes);
+    sb.append(" Minutes ");
+    sb.append(seconds);
+    sb.append(" Seconds");
+
+    return (sb.toString());
+}
